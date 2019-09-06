6aef589b94aa524bee70502d3b340c6015af6206
Eddie-Thuo <thuoeddie@hotmail.co.uk>
2019-09-06 18:38:13 +0100

    add pause icon

--- player-icons.js
+++ player-icons.js
@@ -8,6 +8,9 @@ const template = html`
           <g id="play-arrow">
             <path d="M8 5v14l11-7z"></path>
           </g>
+          <g id="pause">
+            <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
+          </g>
           <g id="replay">
             <path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"></path>
           </g>
