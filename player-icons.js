782afead732cdcb7b18cc3fab80387c5c1b7b30d
Eddie-Thuo <thuoeddie@hotmail.co.uk>
2020-11-22 14:51:27 +0000

    fix indentation

--- player-icons.js
+++ player-icons.js
@@ -6,7 +6,7 @@ const template = html`
       <svg>
         <defs>
           <g id="play-arrow">
-          <path d="M8 5v14l11-7z"></path>
+            <path d="M8 5v14l11-7z"></path>
           </g>
           <g id="pause">
             <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
